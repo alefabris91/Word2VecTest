8dcd9e0c210379d62a2d5b1a2451c4b7a1e83ccf
Corin Mulliss <corin.mulliss@gmail.com>
2016-04-07 17:06:55 +0100

    Added main steps

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Code
+        // Steps
+        // 1. Load data
+        // 2. Preprocess data
+        // 3. Tokenise data
+        // 4. Train the model
+        // 5. Evaluate the model
+        // 6. Visualise model
+        // 7. Saving, reloading and using the model
 
     }
 
